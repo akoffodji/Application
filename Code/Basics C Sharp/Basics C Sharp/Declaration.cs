9af664dd2e410415d18f046c00449da3f8dca1ec
akoffodji <jean.jacques.akoffodji@gmail.com>
2017-03-18 23:04:33 -0400

    Creation du fichier Declaration

---
+++ Code/Basics C Sharp/Basics C Sharp/Declaration.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Basics_C_Sharp
+{
+    abstract class Human
+    {
+        private DateTime dateOfBirth;
+
+        private string firstName;
+
+        private string lastName;
+
+
+        public Human()
+        {
+            dateOfBirth = new DateTime(1992, 9, 17);
+            firstName = "Jean Jacques";
+            lastName = "Akoffodji";
+        }
+
+        public string getFirstName()
+        {
+            return firstName;
+        }
+
+        public string getLastName()
+        {
+            return lastName;
+        }
+
+        public DateTime getDateOfBirth()
+        {
+            return dateOfBirth;
+        }
+
+        public void setFirstName(string value)
+        {
+            firstName = value;
+        }
+
+        public void setLastName(string value)
+        {
+            lastName = value;
+        }
+
+    }
+
+    class Student : Human
+    {
+
+        protected string classTaken;
+
+        public string getClassTaken()
+        {
+            return classTaken;
+        }
+
+        private void setClassTaken(string value)
+        {
+            classTaken = value;
+        }
+    }
+
+    class Employee : Human
+    {
+
+    }
+
+    class Parent : Human
+    {
+
+    }
+
+    class Professor : Employee
+    {
+
+    }
+
+}
