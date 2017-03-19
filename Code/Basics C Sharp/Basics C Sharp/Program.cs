3319c9fe4761bf6951483072670eb2b1a4f9be98
akoffodji <jean.jacques.akoffodji@gmail.com>
2017-03-18 20:44:47 -0400

    Rajout du code pour le C#

--- Code/Basics C Sharp/Basics C Sharp/Program.cs
+++ Code/Basics C Sharp/Basics C Sharp/Program.cs
@@ -10,6 +10,11 @@ namespace Basics_C_Sharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World Aballo Sur le Cloud!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
